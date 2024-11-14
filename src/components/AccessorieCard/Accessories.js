816d99a87898f0d98d1191e1215f37f0c743950a
madhumitasugandhi <madhumitasugandhi@gmail.com>
2024-11-14 16:53:31 +0530

    Accessories.js and Accessories.css created.

---
+++ src/components/AccessorieCard/Accessories.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Accessories.css"
+
+function Accessories() {
+  return (
+    <div>Accessories</div>
+  )
+}
+
+export default Accessories
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -3,7 +3,9 @@ import "./Home.css"
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+        <h1>Sai Car Decore 🚘</h1>
+    </div>
   )
 }
 
